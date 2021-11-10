0808d22dcd43b63381a341ad40987e60b1870145
Jonathan Chu <bobchu351@gmail.com>
2021-11-09 19:44:30 -0800

    implemented loop-based creation of categories
    added reorderable list view and icon buttons

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,9 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> {
+
   final List<String> buttonEntries = ['Daily', 'Weekly', 'Monthly', 'General'];
+  final List<String> textEntries = ['Task 1', 'Task 2', 'Task 3', 'Task 4', 'Task 5'];
   final ButtonStyle headerStyle = ElevatedButton.styleFrom(minimumSize: Size(0, 50));
   var index = 0;
 
@@ -32,67 +34,52 @@ class MyAppState extends State<MyApp> {
         appBar: AppBar(title: Text('TaskMaster')),
         backgroundColor: Colors.white,
         body: ListView(
-          children: <Widget>[
-            ElevatedButton(
-              style: headerStyle,
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  buttonEntries[0],
-                  style: TextStyle(fontSize: 16),
-                  textAlign: TextAlign.center,
-                )
-              ),
-              onPressed: () => print('Answer 1 chosen'),
-            ),
-            ListView(
-              shrinkWrap: true,
-              physics: ClampingScrollPhysics(),
-              children: <Widget>[
-                ListTile(
-                  leading: Icon(Icons.photo_album),
-                  title: Text('Task 1'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.photo_album),
-                  title: Text('Task 2'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.photo_album),
-                  title: Text('Task 2'),
-                ),
-              ],
-            ),
-            ElevatedButton(
-              style: headerStyle,
-              child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    buttonEntries[1],
-                    style: TextStyle(fontSize: 16),
-                    textAlign: TextAlign.center,
-                  )
-              ),
-              onPressed: () {
-                print('Answer 2 chosen');
-              },
-            ),
-            ElevatedButton(
-              style: headerStyle,
-              child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    buttonEntries[2] + ' $index',
-                    style: TextStyle(fontSize: 16),
-                    textAlign: TextAlign.center,
-                  )
-              ),
-              onPressed: printHello,
-            ),
-          ],
+          children: _buildCategories()
         ),
       ),
     );
   }
-
+  Widget _buildCategory(int i) {
+    return AppBar(
+      title: Text(buttonEntries[i] + ' $index'),
+      actions: <Widget>[
+        IconButton(
+            icon: Icon(Icons.access_alarm),
+            tooltip: 'Click here',
+            onPressed: () => print('nice')
+        ),
+      ],
+    );
+  }
+  Widget _buildTasks() {
+    return ReorderableListView(
+        shrinkWrap: true,
+        physics: ClampingScrollPhysics(),
+        children: <Widget>[
+          for (int i = 0; i < textEntries.length; i++)
+            ListTile(
+              key: Key('$i'),
+              trailing: Icon(Icons.agriculture_rounded),
+              title: Text(textEntries[i]),
+            ),
+        ],
+        onReorder: (int oldIndex, int newIndex) {
+          setState(() {
+            if (oldIndex < newIndex) {
+              newIndex -= 1;
+            }
+            final item = textEntries.removeAt(oldIndex);
+            textEntries.insert(newIndex, item);
+          });
+        }
+    );
+  }
+  List<Widget> _buildCategories() {
+    var list = <Widget>[];
+    for (int i = 0; i < buttonEntries.length; i++) {
+      list.add(_buildCategory(i));
+      list.add(_buildTasks());
+    }
+    return list;
+  }
 }
