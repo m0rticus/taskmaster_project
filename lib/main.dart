f320e553708da2132d14702e373c49a24426ef2d
Jonathan Chu <bobchu351@gmail.com>
2021-11-09 22:21:27 -0800

    added basic routing

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/widgets.dart';
 
 void main() => runApp(MyApp());
 
+// Stateful class for app
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -12,33 +13,48 @@ class MyApp extends StatefulWidget {
   }
 }
 
+// Stateful class to build app
+// Primarily for routing
 class MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TaskOverview(),
+      routes: {
+        'categoryDetails': (context) => TaskDetails()
+      }
+    );
+  }
+}
 
+// Class for main homepage overview
+class TaskOverview extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return TaskOverviewState();
+  }
+}
+
+// Stateful class to build homepage overview
+class TaskOverviewState extends State<TaskOverview> {
   final List<String> buttonEntries = ['Daily', 'Weekly', 'Monthly', 'General'];
   final List<String> textEntries = ['Task 1', 'Task 2', 'Task 3', 'Task 4', 'Task 5'];
   final ButtonStyle headerStyle = ElevatedButton.styleFrom(minimumSize: Size(0, 50));
   var index = 0;
 
-  void printHello() {
-    setState(() {
-      index += 1;
-    });
-    print('Button 3 was pressed');
-    print(index);
-  }
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('TaskMaster')),
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('TaskMaster')
+        ),
         backgroundColor: Colors.white,
         body: ListView(
-          children: _buildCategories()
+        children: _buildCategories()
         ),
-      ),
     );
   }
+  // Builds a given category (primarily just the header)
   Widget _buildCategory(int i) {
     return AppBar(
       title: Text(buttonEntries[i] + ' $index'),
@@ -46,11 +62,13 @@ class MyAppState extends State<MyApp> {
         IconButton(
             icon: Icon(Icons.access_alarm),
             tooltip: 'Click here',
-            onPressed: () => print('nice')
+            onPressed: () => Navigator.pushNamed(context, "categoryDetails")
         ),
       ],
     );
   }
+
+  // Builds the tasks in a given category using ReorderableListView.
   Widget _buildTasks() {
     return ReorderableListView(
         shrinkWrap: true,
@@ -59,7 +77,11 @@ class MyAppState extends State<MyApp> {
           for (int i = 0; i < textEntries.length; i++)
             ListTile(
               key: Key('$i'),
-              trailing: Icon(Icons.agriculture_rounded),
+              trailing: IconButton(
+                  icon: Icon(Icons.add),
+                  tooltip: 'More details',
+                  onPressed: () => Navigator.pushNamed(context, "categoryDetails")
+              ),
               title: Text(textEntries[i]),
             ),
         ],
@@ -74,6 +96,8 @@ class MyAppState extends State<MyApp> {
         }
     );
   }
+
+  // Builds the categories of the home page.
   List<Widget> _buildCategories() {
     var list = <Widget>[];
     for (int i = 0; i < buttonEntries.length; i++) {
@@ -81,5 +105,25 @@ class MyAppState extends State<MyApp> {
       list.add(_buildTasks());
     }
     return list;
+  }
+}
+
+// Class for more task details page
+class TaskDetails extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return TaskDetailsState();
+  }
+}
+
+// Class to build task details page
+class TaskDetailsState extends State<TaskDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Task'),
+        )
+    );
   }
 }
